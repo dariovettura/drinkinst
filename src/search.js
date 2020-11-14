f937e350acfa4e5775022de0b283f3e928615d41
Enrico Gigantiello <enrico.gigantiello@leanbakery.com>
2020-11-14 15:36:29 +0100

    dichiara var per variabili

--- src/search.js
+++ src/search.js
@@ -7,10 +7,10 @@ function filterFunction2() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput2");
   filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown2");
+  var div = document.getElementById("myDropdown2");
   a = div.getElementsByClassName("e");
   for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
+    let txtValue = a[i].textContent || a[i].innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       a[i].style.display = "";
     } else {
@@ -24,13 +24,13 @@ function myFunction() {
 }
 
 function filterFunction() {
-  var input, filter, ul, li, a, i;
+  var input, filter, ul, li, a, i, div;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   div = document.getElementById("myDropdown");
   a = div.getElementsByClassName("e");
   for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
+    var txtValue = a[i].textContent || a[i].innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       a[i].style.display = "";
     } else {
